6ecf28fa312b3267457eb561b27211115c4b020c
Chris Wheeler <grintor@gmail.com>
2022-07-20 12:47:59 -0400

    version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="grintor-hooks",
-    version="v0.0.1",
+    version="v0.0.3",
     description="Some pre-commit hooks",
     long_description=README,
     long_description_content_type="text/markdown",
